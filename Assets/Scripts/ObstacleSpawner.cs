cedde9ccf0aa5c74c509acd66c63e6860a34ca40
Ayther <121729473+Aythernix@users.noreply.github.com>
2024-03-18 02:41:23 +0000

    +Made the scripts more readable
    Added regions and comment code to the scripts to make them more readable and easier to understand

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    private int _time;
+    private float _spawnPoint;
+    public GameObject obstacle;
+
+    private float _subtractor = 0;
+    
+    #region Unity Functions
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Calls the "Spawner" function
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Obstacle Spawner
+    IEnumerator Spawner()
+    {
+        // Sets the spawn point to a random number between 9, 3
+        _spawnPoint = Random.Range(9, 3);
+        // Sets the spawn time interval to a random number between 1, 3
+        _time = Random.Range(1, 3);
+        // Waits for the random number between 1,3 and subtracts it by the subtracor
+        yield return new WaitForSeconds(_time - _subtractor);
+
+        // Spawns the top obstacle on the random spawn point
+        Instantiate(obstacle, new Vector3(10.5f, _spawnPoint), quaternion.identity);
+        
+        // Calls the "Subtractor" function
+        StartCoroutine(Subtractor());
+        // Restarts the "Spawner" function
+        StartCoroutine(Spawner());
+    }
+
+    IEnumerator Subtractor() // Makes the subtractor reduce by 0.5
+    {
+        // Waits for 30 seconds
+        yield return new WaitForSeconds(30);
+        // Subtracts the subtractor by 0.5
+        _subtractor =- 0.5f;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/TopObstacle.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TopObstacle : MonoBehaviour
+{
+
+    
+    public GameObject bottomObsatcle;
+    
+
+    private float bottomSpawn;
+    
+    #region Unity Functions
+    // Start is called before the first frame update
+    void Start()
+    {
+        BottomSpawner(); // Calls the "BottomSpawner function
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+    
+    #region Bottom Obstacle Spawn
+    private void BottomSpawner() // Spawns the bottom obstacle
+    {
+        // Calls the "Randomiser" fanction
+        Randomiser(); 
+        
+        // Spawns the bottom obstacle and calls the "Checker" function
+        Instantiate(bottomObsatcle, new Vector3(transform.position.x, bottomSpawn), quaternion.identity, transform);
+        Checker();
+    }
+
+    private void Checker() // Checks if the position of the bottom obstacle is valid
+    {
+        // Runs if the distance between both obstacles is less than 12.4 units or if the X axis of both obstacles don't match
+        if (Vector3.Distance(transform.GetChild(0).position, transform.position) < 12.4 || (int)math.round(transform.GetChild(0).position.x) != (int)math.round(transform.position.x))
+        {
+            // Gets a new position from the "Randomiser" and relocates the bottom obstacle
+            Randomiser();
+            transform.GetChild(0).position = new Vector3(transform.position.x, bottomSpawn);
+            
+            // Checks if the new position is valid
+            Checker();
+            
+            // This function will repeat until a valid position is found
+        }
+        
+        
+    }
+
+    private void Randomiser() // Generates a random number for the spawn position
+    {
+        // Sets the "bottomSpawn" to a random number between -9.2 and -2
+        bottomSpawn = Random.Range(-9.2f, -2);
+    }
+    #endregion
+
+    private void OnTriggerEnter2D(Collider2D col) // runs on collision with a trigger
+    {
+        // Destroys the obstacle
+        Destroy(gameObject); 
+    }
+}
